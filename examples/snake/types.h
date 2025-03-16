2fce7c55e5c078c6770f04a3aee2eac1892ad061
Tarcisio Fischer <tarcisio.fischer.cco@gmail.com>
2025-03-16 18:42:58 +0000

    Move types.h to libgba

--- examples/snake/types.h
+++
@@ -1,9 +0,0 @@
-#ifndef _TYPES_H
-#define _TYPES_H
-
-using u32_t = unsigned int;
-using u16_t = unsigned short;
-using u8_t = unsigned char;
-
-#endif
-
--- examples/tiles/types.h
+++
@@ -1,8 +0,0 @@
-#ifndef _TYPES_H
-#define _TYPES_H
-
-using u16_t = unsigned short;
-using u8_t = unsigned char;
-
-#endif
-
